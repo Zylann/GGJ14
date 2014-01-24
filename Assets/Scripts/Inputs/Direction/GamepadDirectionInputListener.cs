2af29113296c6059b7e4e26e72a4d59a9620b3dd
Theo <torregrossatheo@gmail.com>
2014-01-24 23:51:02 +0100

    WIP direction input listener

---
+++ Assets/Scripts/Inputs/Direction/GamepadDirectionInputListener.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamepadDirectionInputListener : IDirectionInputListener
+{
+	private float _direction;
+
+	public void Initialize()
+	{
+		_direction = 0f;
+	}
+	
+	public void Update()
+	{
+		//_direction = 
+	}
+
+	public float GetDirection()
+	{
+		return _direction;
+	}
+}
---
+++ Assets/Scripts/Inputs/Direction/IDirectionInputListener.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IDirectionInputListener
+{
+	void Initialize();
+
+	void Update();
+
+	/// <summary>
+	/// Gets the player direction
+	/// Positive is forward, negative is backwards
+	/// </summary>
+	/// <returns>The direction</returns>
+	float GetDirection();
+}
