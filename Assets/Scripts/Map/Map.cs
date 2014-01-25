15d59abe07e1887474567d47fc3ee4a8a9bb66a8
Marc <marc.gilleron@gmail.com>
2014-01-25 14:49:31 +0100

    Improved Duck Vision 2000 with OnRenderImage()

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -4,32 +4,76 @@ using System.Collections;
 public class Map : MonoBehaviour
 {
 	public string startMap = "start";
-	private Tilemap _tilemap;
+	public GameObject spikePrefab;
+	public GameObject collectiblePrefab;
+	public Tilemap normalTilemap;
+	public Tilemap duckWorldTilemap;
 
 	void Start()
 	{
 		LoadTilemap(startMap);
 	}
 
-	private void LoadTilemap(string name)
+	private void LoadTilemap(string name, int offsetX = 0)
 	{
+		// Build background
+
+		// Load map data
 		TiledMap tiledMap = new TiledMap();
 		tiledMap.LoadFromJSON(startMap);
-		
-		_tilemap = GetComponent<Tilemap>();
-		_tilemap.collisionMapping = new int[] {
-			1, // block
-			0, // duck
-			2, // spike
+
+		// Build visible tilemap with collisions
+		normalTilemap.collisionMapping = new int[] {
+			1, // [0] block
+			0, // [1] duck
+			2, // [2] spike up
+			3, // [3] coin
 			0, // ...
-			0,
 			0,
 			0,
 			0,
 			0
 		};
-		_tilemap.Build (tiledMap, "background");
-		
+		normalTilemap.Build(tiledMap, "background");
+
+		// Build duckland tilemap without colliders
+		duckWorldTilemap.Build(tiledMap, "background", false);
+		duckWorldTilemap.SetLayer(LayerMask.NameToLayer("DuckWorld"));
+
+		TiledMap.Layer bgLayer = tiledMap.layers["background"];
+
+		// Build special colliders
+
+		for(int y = 0; y < bgLayer.height; ++y)
+		{
+			for(int x = 0; x < bgLayer.width; ++x)
+			{
+				int t = bgLayer.AtYup(x,y); // Note: t starts at index 1
+
+				Vector3 pos = new Vector3(offsetX+x+0.5f, y+0.5f);
+
+				if(t == 3)
+				{
+					Instantiate(spikePrefab, pos, Quaternion.identity);
+				}
+			}
+		}
+
+		// Build collectibles
+
+		TiledMap.Layer objectLayer = tiledMap.layers["objects"];
+		for(int y = 0; y < objectLayer.height; ++y)
+		{
+			for(int x = 0; x < objectLayer.width; ++x)
+			{
+				int t = objectLayer.AtYup(x,y);
+				if(t == 4)
+				{
+					Instantiate(collectiblePrefab, new Vector3(offsetX+x+0.5f, y+0.5f), Quaternion.identity);
+				}
+			}
+		}
+
 	}
 	
 	void Update()
---
+++ Assets/Scripts/Map/Pickup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour
+{
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+}
+
