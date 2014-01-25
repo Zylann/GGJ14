a1fdb597edb93ab50ff47b6aa5e69695df113617
themushroomsound <bonneau.matthieu@gmail.com>
2014-01-25 17:03:05 +0100

    included basic music duckization

---
+++ Assets/Scripts/Audio/DuckizationController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckizationController : MonoBehaviour {
+
+	public float duckizationAmount = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Fabric.EventManager.Instance.SetParameter("Duckable", "Duckization", duckizationAmount);
+	}
+}
