41ad7f165aa31b52912b4fe663a742abb0ec09f8
Marc <marc.gilleron@gmail.com>
2014-01-25 23:25:07 +0100

    Integrated outlined font and score display

---
+++ Assets/Scripts/GUI/GUIScore.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIScore : MonoBehaviour
+{
+	public TextMesh scoreText;
+	public TextMesh scoreTextTransduckified;
+	public Transform scoreObj;
+	private int _displayedScore;
+
+	public TextMesh comboText;
+	public TextMesh comboTextTransduckified;
+	public Transform comboObj;
+	private int _displayedCombo;
+
+	void Start ()
+	{
+		UpdateScoreText(0);
+		UpdateComboText(0);
+	}
+	
+	void Update ()
+	{
+		int score = Game.Inst.m_scoring._current_score;
+		if(score != _displayedScore)
+		{
+			UpdateScoreText(score);
+			_displayedScore = score;
+		}
+
+		int combo = Game.Inst.m_scoring._current_combo;
+		if(_displayedCombo != combo)
+		{
+			UpdateComboText(combo);
+			_displayedCombo = combo;
+		}
+	}
+
+	private void UpdateScoreText(int number)
+	{
+		string numString = ""+number;
+		scoreText.text = numString;
+		scoreTextTransduckified.text = numString;
+	}
+
+	private void UpdateComboText(int number)
+	{
+		string numString = "x"+number;
+		comboText.text = numString;
+		comboTextTransduckified.text = numString;
+	}
+
+}
+
