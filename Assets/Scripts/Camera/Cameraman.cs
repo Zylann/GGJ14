e8e2843e857a40b8317503250bcb517730edd222
Theo <torregrossatheo@gmail.com>
2014-01-25 15:19:02 +0100

    Y scrolling

--- Assets/Scripts/Camera/Cameraman.cs
+++ Assets/Scripts/Camera/Cameraman.cs
@@ -7,6 +7,8 @@ public class Cameraman : MonoBehaviour
 	public GameObject left_block;
 	public float orthographic_size;
 	public float camera_smoothing_factor;
+	public float camera_left_offset = 0.5f;
+	public float up_tolerance = 1f;
 
 	private Camera _camera;
 	private Vector3 _target_position;
@@ -23,11 +25,18 @@ public class Cameraman : MonoBehaviour
 
 	public void Update()
 	{
-		float x_target_position = Game.Inst.m_object_player.transform.position.x + orthographic_size / 2f;
+		float x_target_position = Game.Inst.m_object_player.transform.position.x + orthographic_size * _camera.aspect * camera_left_offset /* + orthographic_size / 2f*/;
 
 		if (x_target_position > transform.position.x)
 		{
-			_target_position = new Vector3 (x_target_position, transform.position.y, transform.position.z);
+			_target_position = new Vector3 (x_target_position, _target_position.y, transform.position.z);
+		}
+		
+		float y_target_position = Game.Inst.m_object_player.transform.position.y;
+
+		if (Mathf.Abs(y_target_position - transform.position.y) > up_tolerance)
+		{
+			_target_position = new Vector3 (_target_position.x, y_target_position, transform.position.z);
 		}
 
 		transform.position = Vector3.Lerp (transform.position, _target_position, camera_smoothing_factor);
