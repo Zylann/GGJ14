0d2eade620819b2f5ec2992482ff074eb4f34464
Marc <marc.gilleron@gmail.com>
2014-01-26 14:07:26 +0100

    Clean code

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -1,6 +1,4 @@
-﻿//#define STREAMED
-
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -8,23 +6,20 @@ public class Map : MonoBehaviour
 {
 	public MapSectorNames[] levels;
 	public GameObject mapSectorPrefab;
-	public int activeSectors = 3;
 	public int levelIndex = 0;
-	private int _sectorIndex;
 
-#if STREAMED
-	private Queue<MapSector> _sectors = new Queue<MapSector>();
-	private MapSector _lastQueuedSector;
-#endif
-
-	void Start ()
+	public void LoadLevel(int index)
 	{
-#if STREAMED
-		AppendSector(0);
-		++_sectorIndex;
-#else
+		Debug.Log("Loading level " + index);
+		levelIndex = index;
 		InstantiateAll();
-#endif
+	}
+	
+	void Start()
+	{
+		Debug.Log("Map.Start()");
+		DontDestroyOnLoad(gameObject);
+		LoadLevel(levelIndex);
 	}
 
 	private void InstantiateAll()
@@ -39,53 +34,6 @@ public class Map : MonoBehaviour
 			newSector.offsetX = offsetX;
 			offsetX += newSector.right;
 		}
-	}
-
-#if STREAMED
-	void Update ()
-	{
-		float avatarX = Game.Inst.m_object_player.transform.position.x;
-		float rightLimit = 50;
-
-		// If the sector on the right is close enough to the avatar
-		if(_lastQueuedSector.right - avatarX < rightLimit)
-		{
-			// If the sector count reached the limit
-			if(_sectors.Count >= activeSectors)
-			{
-				// Erase last sector
-				MapSector lastSector = _sectors.Dequeue();
-				Destroy(lastSector.gameObject);
-			}
-
-			// if the current index has not reached the end of the sectors
-			if(_sectorIndex < levels.Length)
-			{
-				// Append next sector on the right
-				AppendSector(_sectorIndex);
-				++_sectorIndex;
-			}
-		}
-	}
-
-	private void AppendSector(int i)
-	{
-		GameObject sectorObj = Instantiate(mapSectorPrefab) as GameObject;
-		MapSector newSector = sectorObj.GetComponent<MapSector>();
-		newSector.mapName = levels[levelIndex].sectors[i];
-		newSector.offsetX = _lastQueuedSector != null ? _lastQueuedSector.right : 0;
-		_sectors.Enqueue(newSector);
-		_lastQueuedSector = newSector;
-	}
-
-#endif
-
-	private void OnSceneLoad()
-	{
-#if STREAMED
-		_lastQueuedSector = null;
-		_sectors.Clear();
-#endif
 	}
 
 }
