7b1c77b82d5a93e7b75158363f7e82b209d0ef50
Theo <torregrossatheo@gmail.com>
2014-01-26 12:50:34 +0100

    Fixed duckfield clamping

---
+++ Assets/Scripts/Helpers/ScreenHelper.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenHelper : MonoBehaviour
+{
+    [HideInInspector]
+    public int m_screen_width
+    {
+        get;
+        private set;
+    }
+    [HideInInspector]
+    public int m_screen_height
+    {
+        get;
+        private set;
+    }
+    [HideInInspector]
+    public float m_ratio
+    {
+        get;
+        private set;
+    }
+
+    private Resolution m_screen_resolution;
+
+    // Fade to black and logo stuff
+    private Texture2D m_black_texture;
+    private Texture2D m_logo;
+    public enum FADE_STATUS { NORMAL, FADE, LOGO };
+    public FADE_STATUS m_fade_status;
+    private Timer m_timer;
+
+    public float m_fade_black;
+    public float m_fade_logo;
+
+    void Start()
+    {
+        InitializeScreen();
+
+        m_black_texture = Resources.Load("Textures/GUI/black") as Texture2D;
+
+        m_logo = Resources.Load("Textures/GUI/logo_ascent") as Texture2D;
+
+        Screen.showCursor = false;
+    }
+
+    private void InitializeScreen()
+    {
+        m_screen_resolution = Screen.currentResolution;
+        m_screen_width = (int)(m_screen_resolution.width);
+        m_screen_height = (int)(m_screen_resolution.height);
+        m_ratio = m_screen_width / m_screen_height;
+    }
+
+    public float W_to_px(float perc)
+    {
+        return (m_screen_width * perc);
+    }
+
+    public float H_to_px(float perc)
+    {
+        return (m_screen_height * perc);
+    }
+
+    public void StartLogoFade()
+    {
+        m_fade_status = FADE_STATUS.FADE;
+        m_timer = Timer.CreateTimer(m_fade_black);
+    }
+
+    public void OnGUI()
+    {
+        switch (m_fade_status)
+        {
+            case FADE_STATUS.NORMAL:
+
+                break;
+            case FADE_STATUS.FADE:
+                GUI.color = new Color(1f, 1f, 1f, m_timer.GetProgress());
+                GUI.DrawTexture(new Rect(0, 0, m_screen_width, m_screen_height), m_black_texture);
+
+                if (m_timer.HasEnded())
+                {
+                    m_timer.Restart(m_fade_logo);
+                    m_fade_status = FADE_STATUS.LOGO;
+                }
+
+                break;
+            case FADE_STATUS.LOGO:
+
+                GUI.color = new Color(1f, 1f, 1f, 1f);
+                GUI.DrawTexture(new Rect(0, 0, m_screen_width, m_screen_height), m_black_texture, ScaleMode.StretchToFill, true);
+                GUI.color = new Color(1f, 1f, 1f, m_timer.GetProgress());
+                GUI.DrawTexture(new Rect(0, 0, m_screen_width, m_screen_height), m_logo, ScaleMode.StretchToFill, true);
+
+
+                break;
+        }
+    }
+}
--- Assets/Scripts/Player/AnimationDirector.cs
+++ Assets/Scripts/Player/AnimationDirector.cs
@@ -33,11 +33,24 @@ public class AnimationDirector : MonoBehaviour
 			return;
 		}
 
+		if (Game.Inst.m_collision_prober.IsGrounded ())
+		{
+			if (Game.Inst.m_walker.IsMoving())
+			{
+				_animator.Play("walk");
+				_animator.speed = Game.Inst.m_walker.running ? 1.75f : 1.5f;
+				return;
+			}
+			else
+			{
+				_animator.Play("idle");
+				_animator.speed = 1f;
+				return;
+			}
+		}
+
 		if (Game.Inst.m_collision_prober.IsGrounded())
 		{
-			_animator.Play("walk");
-			_animator.speed = Game.Inst.m_walker.running ? 1.75f : Game.Inst.m_walker.IsMoving() ? 1.5f : 0.5f;
-			return;
 		}
 		
 		_animator.Play("walk");
